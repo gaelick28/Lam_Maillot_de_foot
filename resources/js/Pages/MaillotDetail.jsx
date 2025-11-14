88061e908d8c7d199899e792be7c80dc113de742
lijima <john@example.com>
2025-11-14 03:28:44 +0100

    AJOUT ÄÜÖÔ DS PANIER+ MAILLOTDETAIL

--- resources/js/Pages/MaillotDetail.jsx
+++ resources/js/Pages/MaillotDetail.jsx
@@ -32,7 +32,7 @@ export default function MaillotDetail({ maillot, tailles, quantite, prix, prix_n
   };
 
     // Validation identique à Panier.jsx
-    const validateNom = (val) => /^[A-Z'ÇÉÈÊËÏ\s-]*$/.test(val);
+    const validateNom = (val) => /^[A-Z'ÇÉÈÊËÏÄÜÖÔ\s-]*$/.test(val);
   
     // Validation nom : lettres majuscules uniquement
   const handleNomChange = (e) => {
