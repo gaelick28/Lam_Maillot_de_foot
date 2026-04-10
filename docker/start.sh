4aebe17854dc79691ac1ceef780c4ce03e59524c
lijima <john@example.com>
2026-04-11 01:14:30 +0200

    fix: chown www-data sur storage et bootstrap/cache

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+chown -R www-data:www-data /var/www/storage
+chown -R www-data:www-data /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage
 chmod -R 775 /var/www/bootstrap/cache
 
