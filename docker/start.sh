ce1ddadbb88ed1ff7672cd771c6127b2e248c6db
lijima <john@example.com>
2026-04-11 14:33:15 +0200

    fix: chown storage après mkdir dans start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -9,6 +9,8 @@ chmod -R 775 /var/www/bootstrap/cache
 mkdir -p /var/www/storage/framework/views
 mkdir -p /var/www/storage/framework/cache
 mkdir -p /var/www/storage/framework/sessions
+chown -R www-data:www-data /var/www/storage
+chmod -R 775 /var/www/storage
 
 php-fpm -D
 sleep 2
