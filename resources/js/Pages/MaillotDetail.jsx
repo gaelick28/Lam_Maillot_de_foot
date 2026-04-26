64f25444cee9fe84e9bc80b1a1bd61c8bb26a15a
lijima <john@example.com>
2026-04-27 00:38:02 +0200

    Fix: normalisation apostrophe iPhone dans champ nom maillot

--- resources/js/Pages/MaillotDetail.jsx
+++ resources/js/Pages/MaillotDetail.jsx
@@ -61,11 +61,11 @@ export default function MaillotDetail({ maillot, tailles, stocks, quantite, prix
   };
 
     // Validation identique à Panier.jsx
-   const validateNom = (val) => /^[A-ZÀÂÇÉÈÊËÎÏÙÛÜŸÔŒÆÁÓÚÑÃÕÄÖØÅČŠŽĆĐŁ''\s-]*$/.test(val);
+    const validateNom = (val) => /^[A-ZÀÂÇÉÈÊËÎÏÙÛÜŸÔŒÆÁÓÚÑÃÕÄÖØÅČŠŽĆĐŁ'\s-]*$/.test(val);
   
     // Validation nom : lettres majuscules uniquement
   const handleNomChange = (e) => {
-    const val = e.target.value.toUpperCase();
+    const val = e.target.value.toUpperCase().replace(/['']/g, "'");
     if (validateNom(val)&& val.length <= 25) {
       setNom(val);
     }
