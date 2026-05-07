26de3ca230ea8d9666c4c7f0b56d4a267307ab7a
lijima <john@example.com>
2026-05-07 02:50:26 +0200

    Ajout favicon ⚽ en SVG emoji

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @viteReactRefresh
     @vite('resources/js/app.jsx')
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚽</text></svg>">
 </head>
 <body class="bg-gradient-to-r from-blue-800 to-red-500">
     @inertia
