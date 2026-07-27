d1f5dc56f712c3872c8dac954c980436db8c2bb2
lijima <john@example.com>
2026-07-27 05:02:22 +0200

    fix database.php

--- config/database.php
+++ config/database.php
@@ -94,9 +94,7 @@ return [
     'prefix_indexes' => true,
     'schema' => 'public',
     'sslmode' => env('DB_SSLMODE', 'prefer'),
-    'options' => extension_loaded('pdo_pgsql') ? array_filter([
-        PDO::ATTR_EMULATE_PREPARES => true,
-    ]) : [],
+    'options' => [],
 ],
 
         'sqlsrv' => [
